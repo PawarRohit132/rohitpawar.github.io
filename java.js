1cd8acc644e027be20dd48d8a347db1e24536c40
Rohit Pawar <159913960+PawarRohit132@users.noreply.github.com>
2024-03-01 20:21:26 +0530

    Add files via upload
    It is Portfolio Website

---
+++ java.js
@@ -0,0 +1,14 @@
+var burger = document.querySelector(".hamburger");
+var rinav = document.querySelector(".navright");
+
+burger.addEventListener("click", () => {
+  rinav.classList.toggle("h-l-resp");
+  rinav.classList.toggle("v-l-resp");
+});
+// aos animation
+
+
+
+// auto type animation
+
+
